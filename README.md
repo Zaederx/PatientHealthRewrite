ce8c21aa5faaf9a8b4ad5ef940480c0e42fdb39d
ZIshmael <zacharyk.ishmael@gmail.com>
2021-08-21 12:57:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PatientHealth
+ 
---
+++ src/main/java/com/App/PatientHealth/PatientHealthApplication.java
@@ -0,0 +1,13 @@
+package com.App.PatientHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientHealthApplication.class, args);
+	}
+
+}
