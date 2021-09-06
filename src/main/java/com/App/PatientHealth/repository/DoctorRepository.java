aa6a1fdf3780d9111953673ce790bb796a117393
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-06 16:46:06 +0100

    Create DoctorRepository.java

---
+++ src/main/java/com/App/PatientHealth/repository/DoctorRepository.java
@@ -0,0 +1,14 @@
+package com.App.PatientHealth.repository;
+
+import java.util.List;
+
+import com.App.PatientHealth.domain.Doctor;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DoctorRepository extends CrudRepository<Doctor,Integer>{
+    
+    List<Doctor> findByFname(String fname);
+    List<Doctor> findByLname(String lname);
+    
+}
