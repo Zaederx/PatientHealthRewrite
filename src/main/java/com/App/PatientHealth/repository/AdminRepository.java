a56d57672c2a9c4a58dbcb0bfca6ba2542531d8e
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-06 16:46:03 +0100

    Create AdminRepository.java

---
+++ src/main/java/com/App/PatientHealth/repository/AdminRepository.java
@@ -0,0 +1,12 @@
+package com.App.PatientHealth.repository;
+
+import java.util.List;
+
+import com.App.PatientHealth.domain.Admin;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepository extends CrudRepository<Admin,Integer>{
+    List<Admin> findByFname(String fname);
+    List<Admin> findByLname(String lname);
+}
