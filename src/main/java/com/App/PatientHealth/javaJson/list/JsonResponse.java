7c4dcbb7438634762c65673b86bd3d9a8b3223b3
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-06 16:45:50 +0100

    Create JsonResponse.java

---
+++ src/main/java/com/App/PatientHealth/javaJson/list/JsonResponse.java
@@ -0,0 +1,18 @@
+package com.App.PatientHealth.javaJson.list;
+
+
+public class JsonResponse {
+    String message;
+
+    public JsonResponse() {
+        this.message = "";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
