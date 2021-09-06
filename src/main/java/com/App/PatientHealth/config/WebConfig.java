50ee8d317785eaabc9d969bbee27e86ad55345f8
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-06 16:44:32 +0100

    Create WebConfig.java

---
+++ src/main/java/com/App/PatientHealth/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.App.PatientHealth.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration//must have for this class to work
+public class WebConfig implements WebMvcConfigurer{
+ 
+	
+	@Override
+	//must have for images to be served
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+	}
+//
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//		WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
+//	}
+	
+	
+	
+}
