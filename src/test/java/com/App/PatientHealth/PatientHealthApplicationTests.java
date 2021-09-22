c3c33596426ad497b664089fdf935c662dd93f05
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-22 20:03:56 +0100

    Update PatientHealthApplicationTests.java

--- src/test/java/com/App/PatientHealth/PatientHealthApplicationTests.java
+++ src/test/java/com/App/PatientHealth/PatientHealthApplicationTests.java
@@ -4,16 +4,20 @@ package com.App.PatientHealth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -37,7 +41,7 @@ class PatientHealthApplicationTests {
 
 	
 
-	
+
 	public void setup() {
 		mock = MockMvcBuilders
 				.webAppContextSetup(context)
@@ -50,24 +54,63 @@ class PatientHealthApplicationTests {
 	}
 
 	@Test
-	void homePageLoads() throws Exception {
+	void expect_homePageLoads() throws Exception {
 		mock.perform(get(baseUrl+"/")).andExpect(status().isOk());
 	}
 
 	@Test
-	void loginPageLoads() throws Exception {
+	void expect_loginPageLoads() throws Exception {
 		mock.perform(get(baseUrl+"/login")).andExpect(status().isOk());
 	}
 
 	@Test
-	void adminHomeRedirectsToLogin() throws Exception {
+	void expect_adminHomeRedirectsToLogin() throws Exception {
 		mock.perform(get(baseUrl+"/admin/home")).andExpect(redirectedUrl(baseUrl+"/login"));
 	}
 
 	@Test
-	void patientHomeRedirectsToLogin() throws Exception {
+	void expect_patientHomeRedirectsToLogin() throws Exception {
 		mock.perform(get(baseUrl+"/patient/home")).andExpect(redirectedUrl(baseUrl+"/login"));
 	}
+
+	@Test
+	void expect_doctorHomeRedirectsToLogin() throws Exception {
+		mock.perform(get(baseUrl+"/patient/home")).andExpect(redirectedUrl(baseUrl+"/login"));
+	}
+
+	@Test
+	void expect_adminLogsInSuccessfully() throws Exception {
+		mock.perform(post(baseUrl+"/authenticateUser").param("username", "a1").param("password", "password").with(csrf())).andExpect(redirectedUrl("/?loginSuccess"));
+	}
+
+	@Test
+	void expect_patientLogsInSuccessfully() throws Exception {
+		mock.perform(post(baseUrl+"/authenticateUser").param("username", "p1").param("password", "password").with(csrf())).andExpect(redirectedUrl("/?loginSuccess"));
+	}
+
+	@Test
+	void expect_doctorLogsInSuccessfully() throws Exception {
+		mock.perform(post(baseUrl+"/authenticateUser").param("username", "d1").param("password", "password").with(csrf())).andExpect(redirectedUrl("/?loginSuccess"));
+	}
+
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void expect_patientCreated() throws Exception {
+		
+		mock.perform(get(baseUrl+"/admin/register-users")).andExpect(status().isOk()).andExpect(view().name("admin/admin-register-users"));
+		//register a patient
+		mock.perform(post(baseUrl+"/rest/patient/create").contentType(MediaType.APPLICATION_JSON_VALUE).content("{name:\"testingPatientCreated\", username:\"pTestUsername\", email:\"pTestEmail@email.com\", password:\"password\"}").with(csrf())).andExpect(status().isOk());
+	}
+	
+	// @Test
+	// void expect_patientCreated() throws Exception {
+	// 	//login in as admin
+	// 	mock.perform(post(baseUrl+"/authenticateUser").param("username", "a1").param("password", "password").with(csrf())).andExpect(redirectedUrl("/?loginSuccess")).andDo( result ->
+	// 	//got to register users page
+	// 	mock.perform(get(baseUrl+"/admin/register-users")).andExpect(status().isOk()));
+	// 	//register a patient
+	// 	mock.perform(post(baseUrl+"rest/patient/create").contentType(MediaType.APPLICATION_JSON_VALUE).content("{name:\"testingPatientCreated\", username:\"pTestUsername\", email: \"pTestEmail@email.com\" ,password:\"password\"}").with(csrf())).andExpect(status().isOk());
+	// }
 
 	//https://docs.spring.io/spring-security/site/docs/4.0.x/reference/htmlsingle/#running-as-a-user-in-spring-mvc-test-with-annotations
 	// @Test
