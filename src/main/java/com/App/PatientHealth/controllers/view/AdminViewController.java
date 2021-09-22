28c233becd4d09c18654061631948af3b5527108
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-22 20:02:38 +0100

    Create AdminViewController.java

---
+++ src/main/java/com/App/PatientHealth/controllers/view/AdminViewController.java
@@ -0,0 +1,36 @@
+package com.App.PatientHealth.controllers.view;
+
+import com.App.PatientHealth.requestObjects.AdminRegForm;
+import com.App.PatientHealth.requestObjects.DoctorRegForm;
+import com.App.PatientHealth.requestObjects.PatientRegForm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("admin")
+@Controller
+public class AdminViewController {
+
+    @GetMapping("/home")
+    public String adminHome() {
+        return "admin/admin-home";
+    }
+
+    @GetMapping("/register-users")
+    public String registerUsers() {
+        return "admin/admin-register-users";
+    }
+
+    @GetMapping("/search-users")
+    public String searchUsers() {
+        return "admin/admin-search-users";
+    }
+
+    @GetMapping("/doctor/manage-patients")
+    public String manageDoctors() {
+        return "admin-doctor-add-patients";
+    }
+
+}
