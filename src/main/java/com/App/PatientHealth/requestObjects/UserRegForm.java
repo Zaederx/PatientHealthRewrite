dbb63ea1d07debf60b2ffdf051b36a85b935f1ed
ZIshmael <zacharyk.ishmael@gmail.com>
2021-09-22 20:04:19 +0100

    Update UserRegForm.java

--- src/main/java/com/App/PatientHealth/requestObjects/UserRegForm.java
+++ src/main/java/com/App/PatientHealth/requestObjects/UserRegForm.java
@@ -1,24 +1,19 @@
 package com.App.PatientHealth.requestObjects;
 
 public class UserRegForm {
-    // String firstname;
-    // String middlename;
-    // String lastname;
     String name;
     String username;
     String email;
-    String password1;
-    String password2;
+    String password;
 
     public UserRegForm(){}
 
 
-    public UserRegForm(String name, String username, String email, String password1, String password2) {
+    public UserRegForm(String name, String username, String email, String password) {
         this.name = name;
         this.username = username;
         this.email = email;
-        this.password1 = password1;
-        this.password2 = password2;
+        this.password = password;
     }
 
     public String getName() {
@@ -45,19 +40,12 @@ public class UserRegForm {
         this.email = email;
     }
 
-    public String getPassword1() {
-        return this.password1;
+    public String getPassword() {
+        return this.password;
     }
 
-    public void setPassword1(String password) {
-        this.password1 = password;
+    public void setPassword(String password) {
+        this.password = password;
     }
     
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
-    }
 }
