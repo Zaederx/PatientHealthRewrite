a9da3b506254f285662c1bc778fb9237f5bf8de8
ZIshmael <zacharyk.ishmael@gmail.com>
2021-10-13 18:34:18 +0100

    Create Prescription.java

---
+++ src/main/java/com/App/PatientHealth/domain/Prescription.java
@@ -0,0 +1,59 @@
+package com.App.PatientHealth.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.App.PatientHealth.requestObjects.PrescriptionForm;
+
+@Entity
+public class Prescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column
+    String medicationName;
+    @Column
+    String doctorsDirections;
+
+    public Prescription(){}
+    public Prescription(String medicationName, String doctorsDirections) {
+        this.medicationName = medicationName;
+        this.doctorsDirections = doctorsDirections;
+    }
+
+    public Prescription(PrescriptionForm form) {
+        this.medicationName = form.getMedicationName();
+        this.doctorsDirections = form.getDoctorsDirections();
+
+    }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.medicationName;
+    }
+
+    public void setName(String name) {
+        this.medicationName = name;
+    }
+
+    public String getDoctorsDirections() {
+        return doctorsDirections;
+    }
+
+    public void setDoctorsDirections(String doctorsDirections) {
+        this.doctorsDirections = doctorsDirections;
+    }
+
+}
