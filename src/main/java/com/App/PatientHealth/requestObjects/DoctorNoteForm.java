0d7b3901fa635bdfd9340a44521af730973b1cb6
ZIshmael <zacharyk.ishmael@gmail.com>
2021-10-14 12:24:14 +0100

    Create DoctorNoteForm.java

---
+++ src/main/java/com/App/PatientHealth/requestObjects/DoctorNoteForm.java
@@ -0,0 +1,56 @@
+package com.App.PatientHealth.requestObjects;
+
+public class DoctorNoteForm {
+    Integer id;
+    int patientId;
+    String noteHeading;
+    String noteBody;
+
+    public DoctorNoteForm() {
+    }
+
+    /**
+     * 
+     * @param patientId
+     * @param noteHeading
+     * @param noteBody
+     */
+    public DoctorNoteForm(int patientId, String noteHeading, String noteBody) {
+        this.patientId = patientId;
+        this.noteHeading = noteHeading;
+        this.noteBody = noteBody;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNoteHeading() {
+        return this.noteHeading;
+    }
+
+    public void setNoteHeading(String noteHeading) {
+        this.noteHeading = noteHeading;
+    }
+
+    public String getNoteBody() {
+        return this.noteBody;
+    }
+
+    public void setNoteBody(String noteBody) {
+        this.noteBody = noteBody;
+    }
+
+}
