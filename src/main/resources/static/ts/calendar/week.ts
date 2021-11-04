ac991fdca8fb9875105e337ab65a270510b6dde4
ZIshmael <zacharyk.ishmael@gmail.com>
2021-11-04 13:05:14 +0000

    Create week.ts

---
+++ src/main/resources/static/ts/calendar/week.ts
@@ -0,0 +1,19 @@
+class Week {
+    monday:Day;
+    tuesday:Day;
+    wednesday:Day;
+    thursday:Day;
+    friday:Day;
+    saturday:Day;
+    sunday:Day;
+    
+    constructor(monday:Day, tuesday:Day, wednesday:Day, thursday:Day, friday:Day, saturday:Day, sunday:Day) {
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+    }
+}
