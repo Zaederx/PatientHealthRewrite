04448d64db0fee880482d78df468898fc9b4ba13
ZIshmael <zacharyk.ishmael@gmail.com>
2021-11-08 17:23:18 +0000

    Create AppointmentResponse.java

---
+++ src/main/java/com/App/PatientHealth/responseObject/AppointmentResponse.java
@@ -0,0 +1,31 @@
+package com.App.PatientHealth.responseObject;
+
+import com.App.PatientHealth.domain.calendar.Appointment;
+import com.App.PatientHealth.responseObject.domain.AppointmentJson;
+
+public class AppointmentResponse extends JsonResponse {
+    AppointmentJson appointment;
+
+
+    public AppointmentResponse() {}
+
+    public AppointmentResponse(Appointment appointment) {
+        this.appointment = new AppointmentJson(appointment);
+    }
+    public AppointmentResponse(AppointmentJson appointmentJson) {
+        this.appointment = appointmentJson;
+    }
+
+    public AppointmentJson getAppointment() {
+        return this.appointment;
+    }
+
+    public void setAppointment(AppointmentJson appointmentJson) {
+        this.appointment = appointmentJson;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = new AppointmentJson(appointment);
+    }
+
+}
